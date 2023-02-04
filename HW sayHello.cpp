926d5005923ba671d8c850efa101977ad17c55b7
adr400bur@gmail.com <adr400bur@gmail.com>
2023-02-04 12:11:10 -0800

    Add files via upload

---
+++ HW sayHello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+
+	string name;
+
+	cout << "what is your name?" << endl;
+
+	cin >> name;
+
+	//getline(cin, name);
+
+	cout <<"Your name is"<< endl;
+
+	return 0;
+}
